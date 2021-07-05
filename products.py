65c66c965f2befc04c5ef5862feabf9cc7424aef
Wenyi Chien <erin1117.cts04@g2.nctu.edu.tw>
2021-07-05 17:30:36 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,9 @@
+products = []
+
+while True: #不確定存幾次 用while loop
+	name = input('請輸入商品名稱：')
+	if name == 'q': 
+		break
+	price = input('請輸入商品價格:')
+	products.append([name, price]) #大清單裡裝小清單 二維清單
+print(products)
